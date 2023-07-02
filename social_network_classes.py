714456119daa950bb31aafc3cdb8e32d61585dfb
Autumn Fykes <autumn_fykes@brown.edu.com>
2023-07-02 01:32:35 -0400

    update

--- social_network_classes.py
+++ social_network_classes.py
@@ -3,9 +3,19 @@ import hashlib
 # A class to hold general system wide social media data and functions. Eg Data objects of all people, Eg functions: Save social media to disk
 class SocialNetwork:
     def __init__(self):
+              def __init__(self, id, username, age, gender, friends, inbox, list_of_people):
+                self.id = id
+                self.name = username
+                self.year = age
+                self.gender = gender
+                self.friends = []
+                self.inbox = []
+                self.list_of_people = []
+
+    user2 = SocialNetwork(27, "Autumn", 17, "female", [12, 1980, 7])
         # with open('credentials.txt') as file: self.list_of_people = [line.strip() for line in file 
-            self.list_of_people = []# this instance variable is initialized to an empty list when social network is created, 
-                                 # you can save objects of people on the network in this list
+        # this instance variable is initialized to an empty list when social network is created, 
+                         # you can save objects of people on the network in this list
     
     ## For more challenge try this
     def save_social_media(self):
@@ -41,14 +51,17 @@ class SocialNetwork:
 
 
 class Person(object):
-    def __init__(self, id, username, age, gender, friends=None, inbox=None):
+    def __init__(self, id, username, age, gender, friends, inbox):
         self.id = id
         self.name = username
         self.year = age
         self.gender = gender
-        self.friends = friends#[]
-        self.inbox = inbox #[]
-person = Person(27, "Sarah", "Autumn", 21, "female", [11, 1337]) 
+        self.friends = []
+        self.inbox = []
+
+user1 = Person(27, "Autumn", 17, "female", [12, 1980]) 
+
+print(user1)
 
 def is_friend(id1, id2):
     # if (id1 in People.friends) & (id2 in People.friends):
